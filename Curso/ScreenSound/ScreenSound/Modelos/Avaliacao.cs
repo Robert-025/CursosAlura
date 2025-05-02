32e11a4e508810f5fa6681f4864468e6816b98eb
Robert-025 <robert.sena25@gmail.com>
2025-05-02 12:36:49 -0300

    sequencia de curso

---
+++ Curso/ScreenSound/ScreenSound/Modelos/Avaliacao.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Modelos
+{
+    internal class Avaliacao
+    {
+        public Avaliacao(int nota) 
+        {
+            if (nota <= 0){
+
+                Nota = 0;
+
+            }else if (nota > 0 && nota <= 10){
+
+               Nota = nota;
+
+            }else{
+
+               Nota = 10;
+
+            }
+        }
+
+        public int Nota { get; }
+
+        //Metodo static serve para criar funcoes dentro de um tipo
+        public static Avaliacao Parse(string texto)
+        {
+            int nota = int.Parse(texto); 
+            return new Avaliacao(nota);
+        }
+    }
+}
--- Curso/ScreenSound/ScreenSound/Modelos/Banda.cs
+++ Curso/ScreenSound/ScreenSound/Modelos/Banda.cs
@@ -1,9 +1,9 @@
 ﻿namespace ScreenSound.Modelos;
 
-class Banda
+internal class Banda
 {
     private List<Album> albuns = new List<Album>();
-    private List<int> notas = new List<int>();
+    private List<Avaliacao> notas = new List<Avaliacao>();
 
     public Banda(string nome)
     {
@@ -12,7 +12,23 @@ class Banda
 
     public string Nome { get; }
     // Pega a lista de notas criada e usa o Average() -> Método que faz a média automaticamente
-    public double Media => notas.Average();
+    public double Media 
+    { 
+        get
+        {
+            // Se a lista de avaliação não tiver nada (for 0)
+            if (notas.Count == 0)
+            {
+                // Retorna o valor 0
+                return 0;
+            }
+            else
+            {
+                // Retorna cada nota e faz a média
+                return notas.Average(a => a.Nota);
+            }
+        }
+    }
     public List<Album> Albuns => albuns;
 
     public void AdicionarAlbum(Album album) 
@@ -20,7 +36,7 @@ class Banda
         albuns.Add(album);
     }
 
-    public void AdicionarNota(int nota)
+    public void AdicionarNota(Avaliacao nota)
     {
         notas.Add(nota);
     }
